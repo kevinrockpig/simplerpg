cc0974fda7a9180d54651239954b657baf5e3344
Huan <kevinrockpig300@gmail.com>
2020-01-18 13:49:59 +0800

    init commit

---
+++ HW_03.java
@@ -0,0 +1,17 @@
+package homeWork;
+
+import java.util.Scanner;
+
+public class HW_03 {
+	public static void main(String[] args) {
+		int i = 555;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("請輸入三個整數");
+		if(sc.hasNextInt()) {
+			i = 555;
+			System.out.println("三角");		
+			
+		}
+	}
+
+}
